58b0b6a12e06a308889a34da7c6b805c9445ad38
DivyaCD <cd.divya92@gmail.com>
2017-06-05 19:37:22 +0530

    Update hangman.py
    read file with words and print the answer if the player fails to guess in 8 chances

--- hangman.py
+++ hangman.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
+from random import randint
 
 def display():
         for itr in word_arr:
-                if any(itr.lower() in ch.lower() for ch in vowels):
+                if any(itr.lower() in ch for ch in vowels):
                         print('\x1b[6;31;47m'+question[word_arr.index(itr)]+' '+'\x1b[0m'),
                 else:
                         print question[word_arr.index(itr)]+' ',
         print;
 
 def check():
-        if any(letter.lower() in ch.lower() for ch in word_arr):
+        if any(letter in ch for ch in word_arr):
                 for itr in range(len(word_arr)):
-                        if letter.lower()==word_arr[itr].lower():
-                                question[itr]=letter.lower();
+                        if letter==word_arr[itr]:
+                                question[itr]=letter;
                 if question == word_arr:
                         print 'WIN!!!';
                         exit();
@@ -21,9 +22,13 @@ def check():
                 count+=1;
                 print 'You have ',(8-count),' chances to win!'
 
+
 print "~~~~~~~~~~~~~~~~~~~HANGMAN~~~~~~~~~~~~~~~~~~~~";
-word="COMPUTER";
-word_arr=list(word.lower());
+with open("words","r") as file_with_words:
+        words=file_with_words.readlines();
+        random_line=randint(0,len(words));
+        word=words[random_line].replace('\n','').lower();
+word_arr=list(word);
 vowels=('a','e','i','o','u');
 no_of_chances=8;
 count=0;
@@ -32,6 +37,7 @@ question=list(question);
 display();
 
 while(count<no_of_chances):
-        letter=raw_input("Enter the char:")
+        letter=raw_input("Enter the char:").lower();
         check();
         display();
+print "The word is",word
